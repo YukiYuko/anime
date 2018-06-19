6bc3864298cb13422b67fbdf9eebff4c66a5d194
zc <zc@Felix-MBP.lan>
2018-06-19 19:56:32 +0800

    update

---
+++ demo1/js/js.js
@@ -0,0 +1,11 @@
+anime({
+  targets: 'div',
+  translateX: [
+    { value: 100, duration: 1200 },
+    { value: 0, duration: 800 }
+  ],
+  rotate: '1turn',
+  backgroundColor: '#FFF',
+  duration: 2000,
+  loop: true
+});
